3fecf1e20485221eb070b4f0c5963baf240899ad
admin <ahmedbassiouny26@gmail.com>
2019-12-10 19:57:11 +0200

    Initial commit

---
+++ firebaseconfig.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyAfQiaYFvHoAQYrcEegUjQMK5JEUcNLkQw",
+    authDomain: "salem-85549.firebaseapp.com",
+    databaseURL: "https://salem-85549.firebaseio.com/",
+    storageBucket: "gs://salem-85549.appspot.com",
+    };
+
+firebase.initializeApp(config);
